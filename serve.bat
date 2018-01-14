4d6f356bfa9dbea0f327711c7f2045bb0fe4d61d
Cédric Pontet <cedric.pontet@gmail.com>
2018-01-14 17:10:30 +0100

    Incremental

--- serve.bat
+++ serve.bat
@@ -1,3 +1,2 @@
-jekyll serve --watch 
-rem --incremental
+jekyll serve --watch --incremental
 rem docker run --rm --label=jekyll --volume=%CD%:/srv/jekyll  -it -p 4000:4000 jekyll/jekyll:pages jekyll serve
